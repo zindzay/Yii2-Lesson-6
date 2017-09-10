d5624cc33148f6d7dfbe8402bf712796c8646725
Gordeev V.V <zindzay@yandex.com>
2017-09-10 14:13:50 +0500

    first commit

---
+++ views/layouts/main.php
@@ -0,0 +1,24 @@
+<?php
+use yii\helpers\Html;
+
+app\assets\АррlicationUiAssetBundle::register($this);
+?>
+<?php $this->beginPage() ?>
+    <!doctype html>
+    <html lang="<?= Yii::$app->language ?>">
+    <head>
+        <meta charset="<?= Yii::$app->charset ?>">
+        <title><?= Html::encode($this->title) ?></title>
+        <?php $this->head() ?>
+        <?= Html::csrfMetaTags() ?>
+    </head>
+    <body>
+    <?php $this->beginBody() ?>
+    <div class="container">
+        <?= $content; ?>
+        <footer class="footer"><?= Yii::powered() ?></footer>
+    </div>
+    <?php $this->endBody() ?>
+    </body>
+    </html>
+<?php $this->endPage() ?>
